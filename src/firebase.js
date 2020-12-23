55c9f20ed335b48d1c963c44d344bd731ac7a3e2
duygudamlas <40914746+duygudamlas@users.noreply.github.com>
2020-12-23 22:36:04 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
   projectId: "duygudamlas/discord-clone",
   storageBucket: "disrod.netlify.app",
   messagingSenderId: "265446087212",
-  appId: "ea61a877-41d7-41c6-be3e-d7ecfbc66df6",
+  appId: "1:e6e3c9f1-e6d6-46c9-8219-4aec55f8b868",
   measurementId: "G-XQPC1S7QLC"
 };
 // Initialize Firebase
