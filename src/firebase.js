03c83b349cbbeba4410054d77ef54ee90f3debba
duygudamlas <40914746+duygudamlas@users.noreply.github.com>
2020-12-23 22:42:26 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,10 +6,10 @@ import "firebase/analytics";
 
 var firebaseConfig = {
   apiKey: "AIzaSyCmjQ-QuDiTXe3YBU7zJQl0g5q-_mPt9UY",
-  authDomain: "disrod.netlify.app",
-  databaseURL: "https://disrod.netlify.app",
-  projectId: "duygudamlas/discord-clone",
-  storageBucket: "disrod.netlify.app",
+  authDomain: "discord-clone-66d9a.firebaseapp.com",
+  databaseURL: "https://discord-clone-66d9a.firebaseio.com",
+  projectId: "discord-clone-66d9a",
+  storageBucket: "discord-clone-66d9a.appspot.com",
   messagingSenderId: "265446087212",
   appId: "1:265446087212:web:392811aa65680282680f89",
   measurementId: "G-XQPC1S7QLC"
