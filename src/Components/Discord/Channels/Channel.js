c1ce69801c150ce8d7c5560211120f1d437188f1
duygudamlas <40914746+duygudamlas@users.noreply.github.com>
2021-01-02 15:38:59 +0300

    Update Channel.js

--- src/Components/Discord/Channels/Channel.js
+++ src/Components/Discord/Channels/Channel.js
@@ -1,33 +1,32 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-const Channel = ({
-	channel,
-	id,
-	onClick,
-	active,
-	isAdmin,
-	handleDeleteChannel
-}) => {
-	return (
-		<div
-			className={active ? "active channel" : "channel"}
-			onClick={e => {
-				if (!e.target.classList.contains("delete")) {
-					onClick(id);
-				}
-			}}
-		>
-			<span>
-				{channel.type === "text" ? <span className="hash">#</span> : null}{" "}
-				{channel.name}
-			</span>
-			{isAdmin && id !== "0" ? (
-				<span className="delete" onClick={() => handleDeleteChannel()}>
-					&#x2715;
-				</span>
-			) : null}
-		</div>
-	);
-};
+import colors from '../../utils/colors';
+import ChannelName from '../ChannelName';
+
+const StyledChannel = styled.div`
+  margin: 2px 0 2px 8px;
+  padding: 0 8px;
+  height: 32px;
+  background-color: ${props => (props.isSelected ? colors.channelSelected : 0)};
+  border-radius: 3px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  :hover {
+    background-color: ${props => (props.isSelected ? colors.channelSelected : colors.grayLight)};
+
+    span {
+      color: ${props => (props.isSelected ? 0 : colors.channelHoveredText)};
+    }
+  }
+`;
+
+const Channel = ({ name, isSelected, onClick }) => (
+  <StyledChannel isSelected={isSelected} onClick={onClick}>
+    <ChannelName name={name} textColor={isSelected && '#fff'} />
+  </StyledChannel>
+);
 
 export default Channel;
